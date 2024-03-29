c2f51a68d85f50b062d6fb9190cebdd5b8d238bc
201902001 <kmjkmj0020@gmail.com>
2024-03-29 14:20:38 +0900

    edit html

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+var port = 3000;
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + '/home.html');
+});
+app.listen(port, function(){
+    console.log(`Server is running on port :${port}`)
+    }
+);
