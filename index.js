b6498f7b20d67fb6dba217e5cae117ccb4f7c50d
201902001 <kmjkmj0020@gmail.com>
2024-04-01 13:30:49 +0900

    insert url

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-var express = require("express");
+const express = require("express");
+const ejs = require("ejs");
+var bodyparser = require("body-parser");
 
-var app = express();
+const app = express();
 
-var port = 3001;
+const port = 3001;
+
+app.set("view engine", "ejs");
+app.set("views", './view');
+
+app.use(bodyparser.urlencoded({
+    extended: false
+}));
 
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + '/home.html');
+    res.render('index'); // ./view/index.ejs
 });
+app.get("/profile", (req, res)=>{
+    res.render('profile'); // ./view/profile.ejs
+});
+app.get("/login", (req, res)=>{
+    res.render('login'); // ./view/login.ejs
+});
+app.get("/map", (req, res)=>{
+    res.render('map'); // ./view/map.ejs
+});
+app.get("/information", (req, res)=>{
+    res.render('information');
+});
+app.post("/contacProc", (req, res)=>{
+    const name = req.body.name;
+    const phone = req.body.phone;
+    const email = req.body.email;
+    const content = req.body.content;
+
+    var a = `${name} ${phone} ${email} ${content}`;
+    res.send(a);
+})
+
 app.listen(port, function(){
-    console.log(`Server is running on port :${port}`)
+    console.log(`Server is running on port : http://localhost:${port}`)
     }
 );
